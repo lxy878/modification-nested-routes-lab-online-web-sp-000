d3306cc0dc5119972601d926efd46a5a7eefd31a
Xiaoyi Liu <lxy878@gmail.com>
2020-07-07 02:22:51 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -45,7 +45,7 @@ class SongsController < ApplicationController
   def edit
     if !params[:artist_id]
       artist = Artist.find_by(id: params[:artist_id])
-      if artist.nil?
+      if !artist.nil?
         @song = artist.songs.find_by(id: params[:id])
         redirect_to artist_songs_path if !@song.nil?
       else
