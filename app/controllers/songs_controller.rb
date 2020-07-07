e3dff02e51343630f40ce317ae5cd6fbe3fc5f78
Xiaoyi Liu <lxy878@gmail.com>
2020-07-07 02:21:35 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -47,6 +47,7 @@ class SongsController < ApplicationController
       artist = Artist.find_by(id: params[:artist_id])
       if artist
         @song = artist.songs.find_by(id: params[:id])
+        redirect_to artist_songs_path if !@song
       else
         redirect_to artists_path
       end
