361f07e65cb0c43d4e943dd0f8c6be26035ad2ea
Xiaoyi Liu <lxy878@gmail.com>
2020-07-07 02:48:03 +0000

    Automatically backed up by Learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -3,7 +3,6 @@ module SongsHelper
     if song.artist
       hidden_field_tag 'song[artist_id]', song.artist_id
     else
-      p_tag
       collection_select :song, :artist_id, Artist.all, :id, :name
       # select_tag 'song[artist_id]',options_from_collection_for_select(Artist.all, :id, :name)
     end
